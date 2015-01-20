89321d9e7f74ae0507f4bef5955ea61d6cd66d2d
Pieter Joost van de Sande <pj@born2code.net>
2015-01-20 13:48:05 +0100

    adds levigo benchmark

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/jmhodges/levigo"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
 
@@ -25,6 +26,10 @@ func main() {
 	if err := writeWithGoleveldb(max, 2048, value); err != nil {
 		fmt.Printf("goleveldb failed: %v\n", err.Error())
 	}
+	if err := writeWiteLevigo(max, 2048, value); err != nil {
+		fmt.Printf("levigo failed: %v\n", err.Error())
+	}
+
 	// if err := writeWithGocask(max); err != nil {
 	// 	fmt.Printf("gocask failed: %v\n", err.Error())
 	// }
@@ -60,6 +65,55 @@ func writeWithGocask(max int64, value []byte) error {
 
 	duration := time.Since(startedAt)
 	fmt.Printf("gocask: wrote %v msgs in %v, %.0f msgs/s\n", max, duration, float64(max)/duration.Seconds())
+	return nil
+}
+
+func writeWiteLevigo(max int64, batchsize int, value []byte) error {
+	directory, err := ioutil.TempDir("", "levigo_")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("using directory: %v\n", directory)
+	defer os.Remove(directory)
+
+	opts := levigo.NewOptions()
+	opts.SetCache(levigo.NewLRUCache(3 << 30))
+	opts.SetCreateIfMissing(true)
+	defer opts.Close()
+	
+	db, err := levigo.Open(directory, opts)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	sync := levigo.NewWriteOptions()
+	sync.SetSync(true)
+
+	startedAt := time.Now()
+
+	batch := levigo.NewWriteBatch()
+	for key := int64(0); key < max; key++ {
+		batch.Put([]byte{
+			byte(key << 24),
+			byte(key << 16),
+			byte(key << 8),
+			byte(key << 0),
+		}, value)
+
+		if key%int64(batchsize) == 0 {
+			if err := db.Write(sync, batch); err != nil {
+				return err
+			}
+			batch.Clear()
+		}
+	}
+	if err := db.Write(sync, batch); err != nil {
+		return err
+	}
+
+	duration := time.Since(startedAt)
+	fmt.Printf("levigo: wrote %v msgs in %v, %.0f msgs/s\n", max, duration, float64(max)/duration.Seconds())
 	return nil
 }
 
